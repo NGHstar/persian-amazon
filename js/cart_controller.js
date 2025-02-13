a6402eb07d3a7d07a643592c066d09a56b14d06f
NGHSTAR <nghstarcc@gmail.com>
2025-02-13 19:53:26 +0330

    use async instead of promises

--- js/cart_controller.js
+++ js/cart_controller.js
@@ -2,7 +2,10 @@ import { renderOrderSummary } from "./cart/order_summary.js";
 import { renderCheckoutSummary } from "./cart/checkout_summary.js";
 import { fetchProducts } from "./data/products.js";
 
-fetchProducts().then(() => {
+async function loadPage() {
+  await fetchProducts();
   renderOrderSummary();
   renderCheckoutSummary();
-});
+}
+
+loadPage();
